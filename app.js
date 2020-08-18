c7babd39a2fb4ae0b0d4a85ba1b04684886f381e
pushpesh13 <69840044+pushpesh13@users.noreply.github.com>
2020-08-18 12:42:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,17 +3,16 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyCSoy60zE9hQRzGI4Z7TJT0lkfm7wpClus",
+  authDomain: "facebook-d2dac.firebaseapp.com",
+  databaseURL: "https://facebook-d2dac.firebaseio.com",
+  projectId: "facebook-d2dac",
+  storageBucket: "facebook-d2dac.appspot.com",
+  messagingSenderId: "445549538504",
+  appId: "1:445549538504:web:d8c31af007fb4e112b49fd",
+  measurementId: "G-SZ4FEE7KN9"
 };
-
      
  
 ////Do Not Edit Below This
